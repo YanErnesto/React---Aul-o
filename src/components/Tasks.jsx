ee9cca38eea4b0832457ef4c314368c7df1403af
Yan Ernesto <yan.ernesto@rssolutions.com.br>
2026-02-15 22:29:53 -0300

    feat: add check on click task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -20,8 +20,11 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
       {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button onClick={() => onTaskClick(task.id)}
-           className={`bg-slate-400 text-white p-2 text-left rounded-md w-full d-
-            ${task.isCompleted && "line-through"}`}>
+           className={`bg-slate-400 text-white p-2 text-left rounded-md w-full gap-2 flex items-center
+            ${task.isCompleted && "line-through"}`
+            }
+          >
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
  
